4b5e4c2e6fcbfc7fdec3ca67226f3ebafa963ada
Alfredo <icealfredo123@gmail.com>
2022-09-17 08:38:52 -0500

    hi

--- twilio.js
+++ twilio.js
@@ -1,4 +1,4 @@
-const sendMessage = (passed_accountSid , passed_authToken , message) =>{
+const sendMessage = (passed_accountSid, passed_authToken, message, image_url) =>{
   const accountSid = passed_accountSid; // Your Account SID from www.twilio.com/console
   const authToken = passed_authToken; // Your Auth Token from www.twilio.com/console
 
@@ -8,11 +8,14 @@ const sendMessage = (passed_accountSid , passed_authToken , message) =>{
   client.messages
     .create({
       body: message,
+      mediaUrl: [image_url],
+
       to: '+12142288513', // Text this number
       from: '+19032317677', // From a valid Twilio number
     })
     .then((message) => console.log(message.sid));
 
+
+
 }
-sendMessage('ACc5da29b9cb0b3710c2c9386f8a50b026' , '0dfdd4119e53ddf99b40a86e9cbb2b4d' ,
-   "hello wolcome from function call" );
+
